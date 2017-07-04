7e433f819c1edc316af63997849d1f50bba02e5c
Filip Zych <navarionek@gmail.com>
2017-07-04 12:54:34 +0200

    [ADR-184] Fixed old references

--- app/src/main/java/com/slacksocial/android/MainActivity.kt
+++ app/src/main/java/com/slacksocial/android/MainActivity.kt
@@ -2,13 +2,11 @@ package com.slacksocial.android
 
 import android.app.Activity
 import android.os.Bundle
-import co.netguru.android.template.R
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
