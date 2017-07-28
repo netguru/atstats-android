fabd1e7efa8326791f7f65a3cf3f708cc586901b
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2017-07-28 09:56:05 +0200

    Add PaperParcel

--- app/src/main/kotlin/co/netguru/android/socialslack/data/channels/model/Channel.kt
+++ app/src/main/kotlin/co/netguru/android/socialslack/data/channels/model/Channel.kt
@@ -1,11 +1,26 @@
 package co.netguru.android.socialslack.data.channels.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import paperparcel.PaperParcel
 
+@PaperParcel
 data class Channel(val id: String,
-                   val name: String,
-                   @SerializedName("creator") val creatorId: String,
-                   @SerializedName("is_archived") val isArchived: Boolean,
-                   @SerializedName("is_member") val isCurrentUserMember: Boolean,
-                   @SerializedName("num_members") val membersNumber: Int,
-                   var currentPositionInList: Int)
+                                val name: String,
+                                @SerializedName("creator") val creatorId: String,
+                                @SerializedName("is_archived") val isArchived: Boolean,
+                                @SerializedName("is_member") val isCurrentUserMember: Boolean,
+                                @SerializedName("num_members") val membersNumber: Int,
+                                var currentPositionInList: Int) : Parcelable {
+
+    companion object {
+        @JvmField val CREATOR = PaperParcelChannel.CREATOR
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        PaperParcelChannel.writeToParcel(this, dest, flags)
+    }
+
+    override fun describeContents() = 0
+}
