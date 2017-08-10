7f44ec4324a6993e899422217fcfe505e627d044
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-08-10 11:16:07 +0200

    Add DirectChannelsStatistics, create DAO and add to Database
    [ADR-214]

--- app/src/main/kotlin/co/netguru/android/socialslack/data/direct/DirectChannelsDao.kt
+++ app/src/main/kotlin/co/netguru/android/socialslack/data/direct/DirectChannelsDao.kt
@@ -1,15 +1,24 @@
 package co.netguru.android.socialslack.data.direct
 
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import co.netguru.android.socialslack.data.direct.model.DirectChannelStatistics
 import io.reactivex.Single
 
 @Dao
-interface DirectMessagesDao {
+interface DirectChannelsDao {
 
     @Query("SELECT * FROM direct_channel_statistics WHERE channel_id = :channelId")
-    fun getDirectMessageByChannelId(channelId: String): Single<DirectChannelStatistics>
+    fun getDirectChannelStatisticsByChannelId(channelId: String): Single<DirectChannelStatistics>
 
-    @Query("SELECT * FROM direct_channel_statistics WHERE channel_id = :channelId")
+    @Query("SELECT * FROM direct_channel_statistics WHERE user_id = :userId")
+    fun getDirectChannelStatisticsByUserId(userId: String): Single<DirectChannelStatistics>
+
+    @Query("SELECT * FROM direct_channel_statistics")
+    fun getAllDirectChannels(): Single<List<DirectChannelStatistics>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertDirectChannel(directChannelStatistics: DirectChannelStatistics)
 }
