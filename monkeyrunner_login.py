70f97b9b5c55e7e1673d68b8e8dccd349dfcedc7
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-08-14 12:36:28 +0200

    Rewrite the script

--- monkeyrunner_login.py
+++ monkeyrunner_login.py
@@ -1,15 +1,16 @@
 # Imports the monkeyrunner modules used by this program
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+import time
 
 # Connects to the current device
 device = MonkeyRunner.waitForConnection()
 
 # Installs the Android package.
-device.installPackage('app-production-release-bitrise-signed.apk')
+device.installPackage('/bitrise/deploy/app-production-release-bitrise-signed.apk')
 
 # Press 'Ok Got it' dialog
 device.touch(675, 415, MonkeyDevice.DOWN_AND_UP)
-MonkeyRunner.sleep(4)
+time.sleep(5)
 
 # sets a variable with the package's internal name
 package = 'co.netguru.android.socialslack'
@@ -24,45 +25,46 @@ runComponent = package + '/' + activity
 device.startActivity(component=runComponent)
 
 # Wait for the application to open
-MonkeyRunner.sleep(5)
+time.sleep(10)
 print "Application open"
 
 # Press Sign in
-device.touch(360, 950, MonkeyDevice.DOWN_AND_UP)
-MonkeyRunner.sleep(10)
+device.touch(250, 638, MonkeyDevice.DOWN_AND_UP)
 print "Sing in pressed"
+time.sleep(10)
 
 # Click on team field and wait for keyboard
-device.touch(275, 575, MonkeyDevice.DOWN_AND_UP)
-MonkeyRunner.sleep(2)
+device.touch(250, 280, MonkeyDevice.DOWN_AND_UP)
 print "Open browser"
+time.sleep(5)
 
 # Type Slack Team name
 device.type("dyplom101")
+time.sleep(5)
 
 # Press enter key from keyboard
 device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
 print "Input team and next screen"
-MonkeyRunner.sleep(5)
+time.sleep(10)
 
 # Press on the email address field
-device.touch(350, 775, MonkeyDevice.DOWN_AND_UP)
-MonkeyRunner.sleep(2)
+device.touch(250, 390, MonkeyDevice.DOWN_AND_UP)
+time.sleep(5)
 
 # Enter email
 device.type("gonzalo.acosta@netguru.co")
 
 # Press on the password field
-device.touch(350, 550, MonkeyDevice.DOWN_AND_UP)
-MonkeyRunner.sleep(1)
+device.touch(250, 450, MonkeyDevice.DOWN_AND_UP)
+time.sleep(5)
 
 # Enter password
 device.type("qwerty1234")
 
 # Press sign in
-device.touch(350, 700, MonkeyDevice.DOWN_AND_UP)
+device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
 print "Input email and password and next screen"
-MonkeyRunner.sleep(8)
+time.sleep(10)
 
 # Swap down to authorize
 for i in range(1, 10):
@@ -70,17 +72,19 @@ for i in range(1, 10):
     device.touch(300, 0, MonkeyDevice.MOVE)
     print "move down"
     device.touch(300, 400, MonkeyDevice.UP)
-    MonkeyRunner.sleep(0.5)
+    time.sleep(0.5)
 
 # Authorize APP
-device.touch(525, 1075, MonkeyDevice.DOWN_AND_UP)
+device.touch(350, 750, MonkeyDevice.DOWN_AND_UP)
 "Authorize app"
-MonkeyRunner.sleep(3)
+time.sleep(3)
 
 # Select Slack Social APP to open
-device.touch(350, 900, MonkeyDevice.DOWN_AND_UP)
-MonkeyRunner.sleep(5)
+device.touch(250, 650, MonkeyDevice.DOWN_AND_UP)
+time.sleep(5)
 
 # Click on Always
-device.touch(615, 1120, MonkeyDevice.DOWN_AND_UP)
+device.touch(430, 760, MonkeyDevice.DOWN_AND_UP)
 print "Select Slack Social and open app"
+
+time.sleep(60)
