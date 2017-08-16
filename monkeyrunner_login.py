b203aa2b37b9fc5141009a7b4d5ea6dbd735d47d
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-08-16 08:30:05 +0200

    Modify script

--- monkeyrunner_login.py
+++ monkeyrunner_login.py
@@ -6,11 +6,7 @@ import time
 device = MonkeyRunner.waitForConnection()
 
 # Installs the Android package.
-device.installPackage('/bitrise/deploy/app-production-release-bitrise-signed.apk')
-
-# Press 'Ok Got it' dialog
-device.touch(675, 415, MonkeyDevice.DOWN_AND_UP)
-time.sleep(5)
+device.installPackage('app-production-release-bitrise-signed.apk')
 
 # sets a variable with the package's internal name
 package = 'co.netguru.android.socialslack'
@@ -25,38 +21,38 @@ runComponent = package + '/' + activity
 device.startActivity(component=runComponent)
 
 # Wait for the application to open
-time.sleep(10)
 print "Application open"
+time.sleep(30)
 
 # Press Sign in
-device.touch(250, 638, MonkeyDevice.DOWN_AND_UP)
+device.touch(250, 680, MonkeyDevice.DOWN_AND_UP)
 print "Sing in pressed"
-time.sleep(10)
+time.sleep(30)
 
 # Click on team field and wait for keyboard
 device.touch(250, 280, MonkeyDevice.DOWN_AND_UP)
 print "Open browser"
-time.sleep(5)
+time.sleep(10)
 
 # Type Slack Team name
 device.type("dyplom101")
-time.sleep(5)
+time.sleep(30)
 
 # Press enter key from keyboard
 device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
 print "Input team and next screen"
-time.sleep(10)
+time.sleep(40)
 
 # Press on the email address field
 device.touch(250, 390, MonkeyDevice.DOWN_AND_UP)
-time.sleep(5)
+time.sleep(10)
 
 # Enter email
 device.type("gonzalo.acosta@netguru.co")
 
 # Press on the password field
 device.touch(250, 450, MonkeyDevice.DOWN_AND_UP)
-time.sleep(5)
+time.sleep(10)
 
 # Enter password
 device.type("qwerty1234")
@@ -64,7 +60,7 @@ device.type("qwerty1234")
 # Press sign in
 device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
 print "Input email and password and next screen"
-time.sleep(10)
+time.sleep(30)
 
 # Swap down to authorize
 for i in range(1, 10):
@@ -77,14 +73,14 @@ for i in range(1, 10):
 # Authorize APP
 device.touch(350, 750, MonkeyDevice.DOWN_AND_UP)
 "Authorize app"
-time.sleep(3)
+time.sleep(30)
 
 # Select Slack Social APP to open
-device.touch(250, 650, MonkeyDevice.DOWN_AND_UP)
-time.sleep(5)
+device.touch(250, 700, MonkeyDevice.DOWN_AND_UP)
+time.sleep(10)
 
 # Click on Always
 device.touch(430, 760, MonkeyDevice.DOWN_AND_UP)
 print "Select Slack Social and open app"
 
-time.sleep(60)
+time.sleep(10)
