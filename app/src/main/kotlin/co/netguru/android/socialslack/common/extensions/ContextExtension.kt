b4326a7e6f706f4b90d4043bd8f1651137a07e0e
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-08-21 16:46:51 +0200

    Replace drawables for the two themes
    [ADR-166]

--- app/src/main/kotlin/co/netguru/android/socialslack/common/extensions/ContextExtension.kt
+++ app/src/main/kotlin/co/netguru/android/socialslack/common/extensions/ContextExtension.kt
@@ -1,7 +1,9 @@
 package co.netguru.android.socialslack.common.extensions
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.support.annotation.AttrRes
+import android.support.annotation.DrawableRes
 import android.util.TypedValue
 
 
@@ -9,4 +11,10 @@ fun Context.getAttributeColor(@AttrRes attrColor: Int): Int {
     var typedValue = TypedValue()
     this.theme.resolveAttribute(attrColor, typedValue, true)
     return typedValue.data
+}
+
+fun Context.getAttributeDrawable(@AttrRes attrDrawableRes: Int): Int {
+    var typedValue = TypedValue()
+    this.theme.resolveAttribute(attrDrawableRes, typedValue, true)
+    return typedValue.resourceId
 }
