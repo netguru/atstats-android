9d668060796f686399a058533fcbed4487381423
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-08-24 12:22:28 +0200

    Rename user properties
    [ADR-153]

--- app/src/main/kotlin/co/netguru/android/socialslack/data/user/profile/model/UserWithPresence.kt
+++ app/src/main/kotlin/co/netguru/android/socialslack/data/user/profile/model/UserWithPresence.kt
@@ -6,11 +6,11 @@ import co.netguru.android.socialslack.data.user.model.UserDB
 class UserWithPresence(val username: String?,
                        val firstName: String?,
                        val lastName: String?,
-                       val image512: String,
+                       val avatarUrl: String,
                        val presence: Presence) {
 
     companion object {
         fun createUserDBWithPresence(user: UserDB, presence: Presence = Presence.AWAY): UserWithPresence =
-                UserWithPresence(user.username, user.firstName, user.lastName, user.image512, presence)
+                UserWithPresence(user.username, user.firstName, user.lastName, user.avatarUrl, presence)
     }
 }
