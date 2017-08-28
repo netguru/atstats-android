c1620b1b1ae70e1d62a75c385ea7005c80dc41da
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-08-28 08:50:34 +0200

    Remove redundant code
    [ADR-218]

--- app/src/main/kotlin/co/netguru/android/socialslack/feature/home/channels/HomeChannelsViewHolder.kt
+++ app/src/main/kotlin/co/netguru/android/socialslack/feature/home/channels/HomeChannelsViewHolder.kt
@@ -11,18 +11,17 @@ class HomeChannelsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     fun bind(channelStatistics: ChannelStatistics, filter: ChannelsFilterOption) {
         itemView.channelNameTextView.text = channelStatistics.channelName
+        itemView.channelMessagesTitleTextView.text = itemView.context.getText(R.string.messages)
         when (filter) {
             ChannelsFilterOption.MOST_ACTIVE_CHANNEL -> {
                 itemView.channelMessagesTextView.text = channelStatistics.messageCount.toString()
-                itemView.channelMessagesTitleTextView.text = itemView.context.getText(R.string.messages)
             }
             ChannelsFilterOption.CHANNEL_WE_ARE_MOST_ACTIVE -> {
                 itemView.channelMessagesTextView.text = channelStatistics.myMessageCount.toString()
-                itemView.channelMessagesTitleTextView.text = itemView.context.getText(R.string.messages)
             }
             ChannelsFilterOption.CHANNEL_WE_ARE_MENTIONED_THE_MOST -> {
-                itemView.channelMessagesTextView.text = channelStatistics.mentionsCount.toString()
                 itemView.channelMessagesTitleTextView.text = itemView.context.getText(R.string.mentions)
+                itemView.channelMessagesTextView.text = channelStatistics.mentionsCount.toString()
             }
         }
     }
