5b1ba9effe9692d0ef20adcfe2c09a87553069e2
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-08-29 10:28:13 +0200

    Change script

--- monkeyrunner_login.py
+++ monkeyrunner_login.py
@@ -1,86 +1,103 @@
 # Imports the monkeyrunner modules used by this program
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 import time
+import sys
+import signal
+
+device = None
 
 # Connects to the current device
-device = MonkeyRunner.waitForConnection()
+def execute():
+    device = MonkeyRunner.waitForConnection()
 
-# Installs the Android package.
-device.installPackage('/bitrise/deploy/app-production-release-bitrise-signed.apk')
+    # Installs the Android package.
+    device.installPackage('/bitrise/deploy/app-production-release-bitrise-signed.apk')
 
-# sets a variable with the package's internal name
-package = 'co.netguru.android.socialslack'
+    # sets a variable with the package's internal name
+    package = 'co.netguru.android.socialslack'
 
-# sets a variable with the name of an Activity in the package
-activity = 'co.netguru.android.socialslack.feature.splash.SplashActivity'
+    # sets a variable with the name of an Activity in the package
+    activity = 'co.netguru.android.socialslack.feature.splash.SplashActivity'
 
-# sets the name of the component to start
-runComponent = package + '/' + activity
+    # sets the name of the component to start
+    runComponent = package + '/' + activity
 
-# Runs the component
-device.startActivity(component=runComponent)
+    # Runs the component
+    device.startActivity(component=runComponent)
 
-# Wait for the application to open
-print "Application open"
-time.sleep(30)
+    # Wait for the application to open
+    print "Application open"
+    time.sleep(60)
 
-# Press Sign in
-device.touch(250, 680, MonkeyDevice.DOWN_AND_UP)
-print "Sing in pressed"
-time.sleep(50)
+    # Press Sign in
+    device.touch(250, 680, MonkeyDevice.DOWN_AND_UP)
+    print "Sing in pressed"
+    time.sleep(60)
 
-# Click on team field and wait for keyboard
-device.touch(250, 280, MonkeyDevice.DOWN_AND_UP)
-print "Open browser"
-time.sleep(30)
+    # Click on team field and wait for keyboard
+    device.touch(250, 280, MonkeyDevice.DOWN_AND_UP)
+    print "Open browser"
+    time.sleep(60)
 
-# Type Slack Team name
-device.type("dyplom101")
-time.sleep(30)
+    # Type Slack Team name
+    device.type("dyplom101")
+    time.sleep(60)
 
-# Press enter key from keyboard
-device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
-print "Input team and next screen"
-time.sleep(40)
+    # Press enter key from keyboard
+    device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
+    print "Input team and next screen"
+    time.sleep(60)
 
-# Press on the email address field
-device.touch(250, 390, MonkeyDevice.DOWN_AND_UP)
-time.sleep(10)
+    # Press on the email address field
+    device.touch(250, 390, MonkeyDevice.DOWN_AND_UP)
+    time.sleep(60)
 
-# Enter email
-device.type("gonzalo.acosta@netguru.co")
+    # Enter email
+    device.type("gonzalo.acosta@netguru.co")
 
-# Press on the password field
-device.touch(250, 450, MonkeyDevice.DOWN_AND_UP)
-time.sleep(20)
+    # Press on the password field
+    device.touch(250, 450, MonkeyDevice.DOWN_AND_UP)
+    time.sleep(60)
 
-# Enter password
-device.type("qwerty1234")
+    # Enter password
+    device.type("qwerty1234")
 
-# Press sign in
-device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
-print "Input email and password and next screen"
-time.sleep(30)
+    # Press sign in
+    device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
+    print "Input email and password and next screen"
+    time.sleep(60)
 
-# Swap down to authorize
-for i in range(1, 10):
-    device.touch(300, 500, MonkeyDevice.DOWN)
-    device.touch(300, 0, MonkeyDevice.MOVE)
-    print "move down"
-    device.touch(300, 400, MonkeyDevice.UP)
-    time.sleep(0.5)
+    # Swap down to authorize
+    for i in range(1, 10):
+        device.touch(300, 500, MonkeyDevice.DOWN)
+        device.touch(300, 0, MonkeyDevice.MOVE)
+        print "move down"
+        device.touch(300, 400, MonkeyDevice.UP)
+        time.sleep(10)
 
-# Authorize APP
-device.touch(350, 750, MonkeyDevice.DOWN_AND_UP)
-print "Authorize app"
-time.sleep(30)
+    # Authorize APP
+    device.touch(350, 750, MonkeyDevice.DOWN_AND_UP)
+    print "Authorize app"
+    time.sleep(60)
 
-# Select Slack Social APP to open
-device.touch(250, 650, MonkeyDevice.DOWN_AND_UP)
-time.sleep(20)
+    # Select Slack Social APP to open
+    device.touch(250, 650, MonkeyDevice.DOWN_AND_UP)
+    time.sleep(60)
 
-# Click on Always
-device.touch(430, 760, MonkeyDevice.DOWN_AND_UP)
-print "Select Slack Social and open app"
+    # Click on Always
+    device.touch(430, 760, MonkeyDevice.DOWN_AND_UP)
+    print "Select Slack Social and open app"
 
-time.sleep(50)
+    time.sleep(60)
+
+def exit_gracefully(signum, frame):
+    print "Gracefully exiting"
+
+    signal.signal(signal.SIGINT, signal.getsignal(signal.SIGINT))
+    if device is not None:
+        device.shell('killall com.android.commands.monkey')
+    sys.exit(1)
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, exit_gracefully)
+    execute()
