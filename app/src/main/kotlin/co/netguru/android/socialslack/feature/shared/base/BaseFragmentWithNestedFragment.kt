a56653d8592398852d45dd30d862b66907a97d2b
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-09-01 13:26:25 +0200

    Show navigation arrow when showing profile users and profile channels
    [ADR-262]

--- app/src/main/kotlin/co/netguru/android/socialslack/feature/shared/base/BaseFragmentWithNestedFragment.kt
+++ app/src/main/kotlin/co/netguru/android/socialslack/feature/shared/base/BaseFragmentWithNestedFragment.kt
@@ -2,16 +2,25 @@ package co.netguru.android.socialslack.feature.shared.base
 
 import android.support.annotation.IdRes
 import android.support.v4.app.Fragment
+import co.netguru.android.socialslack.feature.main.MainActivity
 
 abstract class BaseFragmentWithNestedFragment : Fragment() {
 
     internal fun replaceNestedFragment(@IdRes containerId: Int, nestedFragment: Fragment) {
+        val mainActivity = activity
+        if (mainActivity is MainActivity) {
+            mainActivity.showMainActionBar()
+        }
         childFragmentManager.beginTransaction()
                 .replace(containerId, nestedFragment)
                 .commit()
     }
 
     internal fun replaceNestedFragmentAndAddToBackStack(@IdRes containerId: Int, nestedFragment: Fragment) {
+        val mainActivity = activity
+        if (mainActivity is MainActivity) {
+           mainActivity.showHomeUpEnableActionBar()
+        }
         childFragmentManager.beginTransaction()
                 .replace(containerId, nestedFragment)
                 .addToBackStack(null)
