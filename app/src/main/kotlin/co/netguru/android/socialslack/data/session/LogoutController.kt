debbca68eff52e9a35497f1617831f0985df9306
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-09-01 10:51:40 +0200

    Change theme when login, log out action on IO
    [ADR-262]

--- app/src/main/kotlin/co/netguru/android/socialslack/data/session/LogoutController.kt
+++ app/src/main/kotlin/co/netguru/android/socialslack/data/session/LogoutController.kt
@@ -2,9 +2,12 @@ package co.netguru.android.socialslack.data.session
 
 import android.content.Context
 import co.netguru.android.socialslack.app.App
+import co.netguru.android.socialslack.common.util.RxTransformers
 import co.netguru.android.socialslack.data.channels.ChannelsDao
 import co.netguru.android.socialslack.data.direct.DirectChannelsDao
 import co.netguru.android.socialslack.data.team.TeamDao
+import co.netguru.android.socialslack.data.theme.ThemeController
+import co.netguru.android.socialslack.data.theme.ThemeOption
 import co.netguru.android.socialslack.data.user.UsersDao
 import io.reactivex.Completable
 import javax.inject.Inject
@@ -17,11 +20,14 @@ class LogoutController @Inject constructor(private val channelsDao: ChannelsDao,
                                            private val usersDao: UsersDao,
                                            private val sessionController: SessionController,
                                            private val loginApi: LoginApi,
+                                           private val themeController: ThemeController,
                                            private val context: Context) {
 
     fun logout(): Completable = removeAllData()
             .mergeWith(sessionController.removeSession())
             .mergeWith(loginApi.revokeToken())
+            .mergeWith(themeController.saveThemeOption(ThemeOption.COLOURFUL))
+            .compose(RxTransformers.applyCompletableIoSchedulers())
             .doOnComplete(this::resetUserComponentAfterLogout)
 
     private fun removeAllData(): Completable = Completable.fromAction({
