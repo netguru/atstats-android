2a372d396a1f1a5a75da0253b943a133c65a25aa
Samuel Urbanowicz <samuel.urbanowicz@netguru.pl>
2017-10-19 14:50:49 +0200

    Update static strings values

--- app/src/main/kotlin/co/netguru/android/atstats/data/filter/model/UsersFilterOption.kt
+++ app/src/main/kotlin/co/netguru/android/atstats/data/filter/model/UsersFilterOption.kt
@@ -5,13 +5,13 @@ import co.netguru.android.atstats.R
 
 enum class UsersFilterOption(@StringRes val textResId: Int) : Filter {
 
-    PERSON_WHO_WE_WRITE_THE_MOST(R.string.person_we_write_most) {
+    PERSON_WHO_WE_WRITE_THE_MOST(R.string.messages_received) {
         override fun filterName() = name
     },
-    PERSON_WHO_WRITES_TO_US_THE_MOST(R.string.person_writes_to_us_most) {
+    PERSON_WHO_WRITES_TO_US_THE_MOST(R.string.messages_sent) {
         override fun filterName() = name
     },
-    PERSON_WHO_WE_TALK_THE_MOST(R.string.person_we_talk_most) {
+    PERSON_WHO_WE_TALK_THE_MOST(R.string.conversations) {
         override fun filterName() = name
     };
 }
